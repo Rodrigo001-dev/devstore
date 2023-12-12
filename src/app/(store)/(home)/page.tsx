23a0daddb9626f191732785cb7082aef187791a1
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-12 08:02:44 -0300

    :sparkles: feat: product details page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,7 +6,11 @@ import { api } from '@/data/api'
 import type { Product } from '@/data/types/product'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
 
   const products = await response.json()
 
