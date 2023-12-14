480d68afd284f4f54d87bae16d431f1973875424
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-14 17:30:38 -0300

    :sparkles: feat: open graph image

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -2,11 +2,15 @@ import type { ReactNode } from 'react'
 
 import { Header } from '@/components/header'
 
+import { CartProvider } from '@/contexts/cart-context'
+
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
